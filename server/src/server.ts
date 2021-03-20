54f6068e0614a065b0a6188f0d227d4a6a26f37f
shaniacorbishley2 <44789714+shaniacorbishley2@users.noreply.github.com>
2021-03-20 15:36:20 +0000

    Added server setup

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+// src/server.ts
+import * as express from "express";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+var http = require("http").Server(app);
+
+// simple '/' endpoint sending a Hello World
+// response
+app.get("/", (req: any, res: any) => {
+  res.send("hello world");
+});
+
+// start our simple server up on localhost:3000
+const server = http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
