fceda9c7f008720d9757cac9b0c8ff7ae182058b
shaniacorbishley2 <44789714+shaniacorbishley2@users.noreply.github.com>
2021-04-25 19:29:34 +0100

    Add js builds

---
+++ server/src/enums/movement.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var Movement;
+(function (Movement) {
+    Movement[Movement["None"] = 0] = "None";
+    Movement["IdleLeft"] = "IDLE_LEFT";
+    Movement["IdleRight"] = "IDLE_RIGHT";
+    Movement["Left"] = "LEFT";
+    Movement["Right"] = "RIGHT";
+    Movement["JumpLeft"] = "JUMP_LEFT";
+    Movement["JumpRight"] = "JUMP_RIGHT";
+    Movement["SideJumpLeft"] = "SIDE_JUMP_LEFT";
+    Movement["SideJumpRight"] = "SIDE_JUMP_RIGHT";
+})(Movement || (Movement = {}));
+exports["default"] = Movement;
+//# sourceMappingURL=movement.js.map
